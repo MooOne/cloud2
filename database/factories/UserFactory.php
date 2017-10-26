7b4c1a8c1cb220d09a06277596d015f01956d380
lixiaodong <lixiaodong@yeelight.com>
2017-10-26 11:48:39 +0800

    database migrations

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,7 +13,7 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(Yeelight\User::class, function (Faker $faker) {
+$factory->define(\Yeelight\Models\Foundation\User::class, function (Faker $faker) {
     static $password;
 
     return [
