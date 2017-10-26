8bdf9fdb6367c671a78878797233eac9ca0ddaea
lixiaodong <lixiaodong@yeelight.com>
2017-10-26 14:58:12 +0800

    test 目录

---
+++ tests/Api/BasicTest.php
@@ -0,0 +1,38 @@
+<?php
+namespace Tests\Api;
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class BasicTest extends BaseApiTestCase
+{
+
+    public function testPhoneNumber()
+    {
+        $phoneNumberModel = phone_model_from("+6590123456", "SG");
+        print_r($phoneNumberModel->toArray());
+
+        // is valid number
+        $this->assertTrue($phoneNumberModel->isValid());
+
+        // is valid mobile number
+        $this->assertTrue($phoneNumberModel->isMobileNumber());
+    }
+
+    public function testCountry()
+    {
+
+        // country list
+        $localizedCountryList = \Yeelight\Models\Basic\CountryModel::getLocalizedCountryList();
+        print_r($localizedCountryList);
+
+        // country list
+        $countryInfoIndexedList = \Yeelight\Models\Basic\CountryModel::getCountryInfoIndexedList();
+        print_r($countryInfoIndexedList);
+
+    }
+
+
+
+}
---
+++ tests/Unit/ModelTest.php
@@ -0,0 +1,20 @@
+<?php
+namespace Tests\Unit;
+
+use Yeelight\Models\Foundation\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ModelTest extends TestCase
+{
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        factory(User::class)->create();
+    }
+}
