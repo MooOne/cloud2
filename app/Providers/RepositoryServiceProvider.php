b65bdb36a56ff237facee6a0923c9cb85411cf0b
lixiaodong <lixiaodong@yeelight.com>
2017-10-26 19:04:28 +0800

    repository service provider 自动加载

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -14,7 +14,9 @@ class RepositoryServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->mergeConfigFrom(base_path('vendor/prettus/l5-repository/src/resources/config/repository.php'), 'repository');
+
+        $this->loadTranslationsFrom(base_path('vendor/prettus/l5-repository/src/resources/lang'), 'repository');
     }
 
     /**
@@ -24,7 +26,27 @@ class RepositoryServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->commands('Yeelight\Repositories\Generators\Commands\RepositoryCommand');
+        $this->commands('Yeelight\Repositories\Generators\Commands\TransformerCommand');
+        $this->commands('Yeelight\Repositories\Generators\Commands\PresenterCommand');
+        $this->commands('Yeelight\Repositories\Generators\Commands\EntityCommand');
+        $this->commands('Yeelight\Repositories\Generators\Commands\ValidatorCommand');
+        $this->commands('Yeelight\Repositories\Generators\Commands\ControllerCommand');
+        $this->commands('Yeelight\Repositories\Generators\Commands\BindingsCommand');
+        $this->commands('Yeelight\Repositories\Generators\Commands\CriteriaCommand');
+
         $this->app->bind(UserRepository::class, UserRepositoryEloquent::class);
+        $this->app->bind(\Yeelight\Repositories\Interfaces\PostRepository::class, \Yeelight\Repositories\Eloquent\PostRepositoryEloquent::class);
         //:end-bindings:
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
     }
 }
