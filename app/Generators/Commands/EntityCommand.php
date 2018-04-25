b37ac2bbb56707fedf29250ed15fdfaf04767704
lixiaodong <lixiaodong@yeelight.com>
2018-04-25 14:50:40 +0800

    entity 命令里包含view和lang，api_controller

--- app/Generators/Commands/EntityCommand.php
+++ app/Generators/Commands/EntityCommand.php
@@ -71,6 +71,33 @@ class EntityCommand extends CommandBase
             ]);
         }
 
+        if ($this->confirm('Would you like to create a API Controller? [y|N]')) {
+
+            // Generate a api controller resource
+            $this->call('yl:api_controller', [
+                'name' => $this->argument('name'),
+                '--force' => $this->option('force')
+            ]);
+        }
+
+        if ($this->confirm('Would you like to create CURD views? [y|N]')) {
+
+            // Generate a controller resource
+            $this->call('yl:views', [
+                'name' => $this->argument('name'),
+                '--force' => $this->option('force')
+            ]);
+        }
+
+        if ($this->confirm('Would you like to create language package? [y|N]')) {
+
+            // Generate a controller resource
+            $this->call('yl:lang', [
+                'name' => $this->argument('name'),
+                '--force' => $this->option('force')
+            ]);
+        }
+
         $this->call('yl:repository', [
             'name' => $this->argument('name'),
             '--fillable' => $this->option('fillable'),
